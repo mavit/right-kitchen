6e36ee303b8afe69703091a782e8f20c1ee39c4c
Peter Oliver <git@mavit.org.uk>
2013-09-03 18:25:37 +0100

    Better parsing. Tweaked report.

--- right-kitchen.pl
+++ right-kitchen.pl
@@ -13,29 +13,34 @@ my $rss_url = shift;
 my $rss = XML::LibXML->load_xml(
     'location' => $rss_url,
 );
-foreach my $link_node ( $rss->findnodes('//item/link') ) {
-    my $printed = 0;
 
+foreach my $link_node (
+    $rss->findnodes('//item[category[text() != "Rightmove Search"]]/link')
+) {
+    my @report = ();
     my $html = XML::LibXML->load_html(
         'location' => $link_node->textContent,
         'recover' => 1,
         'encoding' => 'Windows-1252',	# FIXME: override only ISO-8859-1.
     );
-    foreach my $p (
-        $html->findnodes('//div[@class="propertyDetailDescription"]/p')
-    ) {
-        if ( $p =~ m/kitchen.+(\d+\.\d+)\s*m(?:etres?)?\s*\S?\s*(\d+\.\d+)\s*m(?:etres?)?/i ) {
-            my ($x, $y) = ($1, $2);
-            print $x * $y, " m² (${x} m × ${y} m)\n";
 
-            my $desc = $html->findnodes('//div[@id="propertyAddress"]')
-                ->[0]->textContent;
-            $desc =~ s/^\s*//mg;
-            print $desc, $link_node->textContent, "\n\n";
-            $printed = 1;
+    foreach my $detail (
+        $html->findnodes('//div[@class="propertyDetailDescription"]')
+    ) {
+        if ( $detail =~ m/.*(?:^|>)(.*?kitchen[^<]{0,12}(?:[\s\d[:punct:]x]|yds|(?:min|max)(?:imum)?|<[^>]*>)*?(\d+(?:\.\d+)?)\s*m(?:etres?)?(?:[\s\d[:punct:]x]|yds|(?:min|max)(?:imum)?)*?(\d+(?:\.\d+)?)\s*m(?:etres?)?)/im ) {
+            my ($text, $x, $y) = ($1, $2, $3);
+            push @report, $x * $y. " m²\t($text)\n";
         }
     }
+    
+    my $desc = $html->findnodes('//div[@id="propertyAddress"]')
+        ->[0]->textContent;
+    $desc =~ s/^\s*//mg;
 
-    print "FIXME: Didn't find a kitchen for ", $link_node->textContent, "\n\n"
-        unless $printed;
+    print(
+        $desc,
+        (@report ? @report : "Couldn't find a kitchen.\n"),
+        $link_node->textContent,
+        "\n\n"
+    );
 }
